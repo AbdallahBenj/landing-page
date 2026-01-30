e3f6146dc7b9a721a4f6794dda92046960035406
AbdallahBenj <abdallahbenjalal@gmail.com>
2026-01-30 18:28:40 +0100

    fix scroll to views section bug

--- src/components/ScrollToSection.js
+++ src/components/ScrollToSection.js
@@ -1,12 +1,9 @@
 const ScrollToSection = (section) => {
   if (section) {
-    section?.scrollIntoView(
-      {
-        behavior: "smooth",
-        block: "start",
-      },
-      100,
-    );
+    section?.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    }, 100);
   } else if (!section) {
     console.warn("There is no section!");
   }
